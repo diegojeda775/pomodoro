3cbf81951b925895d20cdd003d4755f848bb3ecf
Diego Ojeda Rivera <diegojeda775@yahoo.com>
2021-03-20 19:26:36 -0700

    change json

---
+++ build/precache-manifest.4d46b4767526b77be5fed0c6816e4e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaaf2ecd3b6e8f8968d112461ebc9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500ab6f6de80e908604e",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500ab6f6de80e908604e",
+    "url": "/static/js/main.26a34c32.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
